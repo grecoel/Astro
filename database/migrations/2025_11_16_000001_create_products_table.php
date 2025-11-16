3cb202baec3942ac17af391c157914b98be3b39c
Gege <110276009+grecoel@users.noreply.github.com>
2025-11-16 16:06:56 +0700

    update migrasi table products

--- database/migrations/2025_11_16_000001_create_products_table.php
+++ database/migrations/2025_11_16_000001_create_products_table.php
@@ -3,21 +3,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migrations
+return new class extends Migration
 {
     public function up():void {
-        Schema::create('products', function (Blueprint $table)){
-            $table->string('id');
+        Schema::create('products', function (Blueprint $table){
+            $table->uuid('id')->primary();  
             $table->string('name');
             $table->longText('description')->nullable();
-            $table->double('price',10 ,2);
-            $table->string('imageURL');
+            $table->decimal('price',10 ,2);
+            $table->string('image_url');
             $table->string('condition');  
 
-            $table->foreignId('seller_id')->constrained('sellers')->onDelete('cascade');
-            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->foreignUuid('seller_id')->constrained('sellers')->onDelete('cascade');
+            $table->foreignUuid('category_id')->constrained('categories')->onDelete('cascade');
 
             $table->timestamps();
         }
+    );
+    }
+
+    public function down():void{
+        Schema::dropIfExist('products');
     }
 }
