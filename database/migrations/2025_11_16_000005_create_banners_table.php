660db264a75c59cde129a2b2846b0d54ddf065e2
Erzachel Senna Rizky Farezi <sennarizkyfarezi@gmail.com>
2025-11-16 16:33:42 +0700

    buat migrasi dan model banner

---
+++ database/migrations/2025_11_16_000005_create_banners_table.php
@@ -0,0 +1,25 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up():void {
+        Schema::create('banners', function (Blueprint $table){
+            $table->uuid('id')->primary();  
+            $table->string('title');
+            $table->string('image_url');
+            $table->string('link_url');
+            $table->string('condition');
+            $table->boolean('is_active')->default(true);
+
+            $table->timestamps();
+        }
+    );
+    }
+
+    public function down():void{
+        Schema::dropIfExists('banners');
+    }
+};
